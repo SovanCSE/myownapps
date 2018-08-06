04a0ab34d76d4b4cdaf22e0862c8f3f2b32bc409
SovanIT <sovan@trisysit.com>
2018-08-07 00:12:30 +0530

    urls in firstapp is changed.

--- firstapp/urls.py
+++ firstapp/urls.py
@@ -1,7 +1,6 @@
-
-from django.contrib import admin
 from django.conf.urls import include,url
 from firstapp import views
+
 urlpatterns = [
     url('^$',views.login_page,name='firstapp.login_page')
     
